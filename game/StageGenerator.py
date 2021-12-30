38bc01a4826eb3ff52c5ecec99bcd7efa61f39c0
Yiliu <1094440632@qq.com>
2021-12-30 12:46:16 +0800

    add stage 1

---
+++ game/StageGenerator.py
@@ -0,0 +1,42 @@
+from core import *
+from display import *
+import numpy as np
+from danmaku import *
+from enemies import *
+
+
+data = StageData.empty('S1',10*60*60)
+# 0 ~ 1000
+data.insert(0, AimmingEnemy3(100, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+data.insert(9, AimmingEnemy3(110, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+data.insert(18, AimmingEnemy3(120, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+data.insert(27, AimmingEnemy3(130, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+data.insert(36, AimmingEnemy3(140, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+data.insert(45, AimmingEnemy3(150, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+data.insert(54, AimmingEnemy3(160, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+data.insert(63, AimmingEnemy3(170, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+data.insert(72, AimmingEnemy3(180, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+data.insert(81, AimmingEnemy3(190, 100, shoot_time = 10, bullet_speed=10, arc = np.pi / 24, alive_time=10*60))
+
+# 1000 ~ 1500
+data.insert(1000, AimmingEnemy(100, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1020, AimmingEnemy(100, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1040, AimmingEnemy(100, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1060, AimmingEnemy(100, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1080, AimmingEnemy(100, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1100, AimmingEnemy(100, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1000, AimmingEnemy(540, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1020, AimmingEnemy(540, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1040, AimmingEnemy(540, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1060, AimmingEnemy(540, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1080, AimmingEnemy(540, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+data.insert(1100, AimmingEnemy(540, 10, shoot_time = 2, bullet_speed=10, alive_time=3*60, vy = 3))
+
+# 1500 ~ 2500
+data.insert(1500, RandomEnemy(320, 100, shoot_time = 5, bullet_speed=5, alive_time=7*60))
+
+# 2500 ~ 3500
+data.insert(2500, RandomEnemyWithFall(320, 100, shoot_time = 5, bullet_speed=5, alive_time=7*60))
+
+G = game_with_play(Player(320, 400), data)
+print(G.play())
--- game/enemies.py
+++ game/enemies.py
@@ -2,17 +2,18 @@ from core import *
 from danmaku import *
 
 class AimmingEnemy(Enemy):
-    def __init__(self, x, y, w = 5, h = 5, vx = 0, vy = 0, shoot_time = 20, alive_time = 5 * 60):
+    def __init__(self, x, y, w = 5, h = 5, vx = 0, vy = 0, shoot_time = 20, alive_time = 5 * 60, bullet_speed = 2):
         super().__init__(x, y, w, h, vx, vy)
         self.alive_time = alive_time
         self.shoot_time = shoot_time
+        self.bullet_speed = bullet_speed
 
     def get_arc(self, player):
         x = self.x - player.x
         y = self.y - player.y
         return np.arctan2(y, x) - np.pi
     
-    def shoot(self, player, v = 20):
+    def shoot(self, player, v = 2):
         arc = self.get_arc(player)
         return [obj_bullet(self.x, self.y, 4, 4, v * np.cos(arc), v * np.sin(arc), 0, 0)]
     
@@ -22,25 +23,27 @@ class AimmingEnemy(Enemy):
         if self.alive_time <= 0:
             self.alive_time = 0
         if self.alive_time % self.shoot_time == 0:
-            return self.shoot(player)
+            return self.shoot(player, self.bullet_speed)
 
 class AimmingEnemy3(Enemy):
-    def __init__(self, x, y, w = 5, h = 5, vx = 0, vy = 0, shoot_time = 20, alive_time = 5 * 60):
+    def __init__(self, x, y, w = 5, h = 5, vx = 0, vy = 0, shoot_time = 20, alive_time = 5 * 60, bullet_speed = 2, arc = np.pi / 12):
         super().__init__(x, y, w, h, vx, vy)
         self.alive_time = alive_time
         self.shoot_time = shoot_time
+        self.bullet_speed = bullet_speed
+        self.arc = arc
 
     def get_arc(self, player):
         x = self.x - player.x
         y = self.y - player.y
         return np.arctan2(y, x) - np.pi
     
-    def shoot(self, player, v = 20):
+    def shoot(self, player, v = 2):
         arc = self.get_arc(player)
         bullets = []
         bullets.append(obj_bullet(self.x, self.y, 4, 4, v * np.cos(arc), v * np.sin(arc), 0, 0))
-        bullets.append(obj_bullet(self.x, self.y, 4, 4, v * np.cos(arc + np.pi / 12), v * np.sin(arc + np.pi / 12), 0, 0))
-        bullets.append(obj_bullet(self.x, self.y, 4, 4, v * np.cos(arc - np.pi / 12), v * np.sin(arc - np.pi / 12), 0, 0))
+        bullets.append(obj_bullet(self.x, self.y, 4, 4, v * np.cos(arc + self.arc), v * np.sin(arc + np.pi / 12), 0, 0))
+        bullets.append(obj_bullet(self.x, self.y, 4, 4, v * np.cos(arc - self.arc), v * np.sin(arc - np.pi / 12), 0, 0))
         return bullets
     
     def update(self, player):
@@ -49,20 +52,21 @@ class AimmingEnemy3(Enemy):
         if self.alive_time <= 0:
             self.alive_time = 0
         if self.alive_time % self.shoot_time == 0:
-            return self.shoot(player)
+            return self.shoot(player, self.bullet_speed)
 
 class AimmingEnemy14(Enemy):
-    def __init__(self, x, y, w = 5, h = 5, vx = 0, vy = 0, shoot_time = 20, alive_time = 5 * 60):
+    def __init__(self, x, y, w = 5, h = 5, vx = 0, vy = 0, shoot_time = 20, alive_time = 5 * 60, bullet_speed = 2):
         super().__init__(x, y, w, h, vx, vy)
         self.alive_time = alive_time
         self.shoot_time = shoot_time
+        self.bullet_speed = bullet_speed
 
     def get_arc(self, player):
         x = self.x - player.x
         y = self.y - player.y
         return np.arctan2(y, x) - np.pi
     
-    def shoot(self, player, v = 20):
+    def shoot(self, player, v = 2):
         arc = self.get_arc(player)
         bullets = []
         arc -= np.pi * 7 / 18
@@ -77,4 +81,52 @@ class AimmingEnemy14(Enemy):
         if self.alive_time <= 0:
             self.alive_time = 0
         if self.alive_time % self.shoot_time == 0:
-            return self.shoot(player)
+            return self.shoot(player, self.bullet_speed)
+
+class RandomEnemy(Enemy):
+    def __init__(self, x, y, w = 5, h = 5, vx = 0, vy = 0, shoot_time = 20, alive_time = 5 * 60, bullet_speed = 2):
+        super().__init__(x, y, w, h, vx, vy)
+        self.alive_time = alive_time
+        self.shoot_time = shoot_time
+        self.bullet_speed = bullet_speed
+
+    def shoot(self, v = 2):
+        bullets = []
+        arc = np.random.rand() * 2 * np.pi
+        per_arc = np.pi / 24
+        for i in range(48):
+            bullets.append(obj_bullet(self.x, self.y, 4, 4, v * np.cos(arc), v * np.sin(arc), 0, 0))
+            arc += per_arc
+        return bullets
+    
+    def update(self, player):
+        super().update()
+        self.alive_time -= 1
+        if self.alive_time <= 0:
+            self.alive_time = 0
+        if self.alive_time % self.shoot_time == 0:
+            return self.shoot(self.bullet_speed)
+
+class RandomEnemyWithFall(Enemy):
+    def __init__(self, x, y, w = 5, h = 5, vx = 0, vy = 0, shoot_time = 20, alive_time = 5 * 60, bullet_speed = 2):
+        super().__init__(x, y, w, h, vx, vy)
+        self.alive_time = alive_time
+        self.shoot_time = shoot_time
+        self.bullet_speed = bullet_speed
+
+    def shoot(self, v = 2):
+        bullets = []
+        arc = np.random.rand() * 2 * np.pi
+        per_arc = np.pi / 24
+        for i in range(48):
+            bullets.append(obj_bullet(self.x, self.y, 4, 4, v * np.cos(arc), v * np.sin(arc), 0, 0.15))
+            arc += per_arc
+        return bullets
+    
+    def update(self, player):
+        super().update()
+        self.alive_time -= 1
+        if self.alive_time <= 0:
+            self.alive_time = 0
+        if self.alive_time % self.shoot_time == 0:
+            return self.shoot(self.bullet_speed)
